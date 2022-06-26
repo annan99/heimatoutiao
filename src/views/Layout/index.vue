3993e9196a14fd9eb49ff8e766b4981169046023
annan <2029778094@qq.com>
2022-06-26 11:44:43 +0800

    路由配置完成

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,17 @@
 <template>
 <div>
-    父页面
+    <router-view />
+
+<van-tabbar route>
+  <van-tabbar-item replace to="/home" icon="home-o"
+  >首页<i class="toutiao toutiao-shouye" slot="icon"></i></van-tabbar-item>
+  <van-tabbar-item replace to="/video" icon="search">视频
+  <i class="toutiao toutiao-shipin" slot="icon"></i></van-tabbar-item>
+  <van-tabbar-item replace to="/question" icon="search">问答
+  <i class="toutiao toutiao-wenda" slot="icon"></i></van-tabbar-item>
+  <van-tabbar-item replace to="/my" icon="search">我的
+  <i class="toutiao toutiao-wode" slot="icon"></i></van-tabbar-item>
+</van-tabbar>
 </div>
 </template>
 
@@ -18,6 +29,11 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.toutiao{
+    font-size: 40px;
+}
+/deep/ .van-tabbar-item_text{
+    font-size: 20px !;
+}
 </style>
